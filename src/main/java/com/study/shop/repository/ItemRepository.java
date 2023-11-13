ed4d18a63ffeb8b1a8115c063e03fdc11431aa88
Hyeju <kimoju03@naver.com>
2023-11-13 19:27:04 +0900

    test: JPQL, QueryDSL 상품 설명 조회 쿼리 테스트

--- src/main/java/com/study/shop/repository/ItemRepository.java
+++ src/main/java/com/study/shop/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package com.study.shop.repository;
 
 import com.study.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -9,4 +11,9 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
 
     List<Item> findByItemName(String itemName);
 
+    // JPQL. 데이터베이스 종류에 상관 없다.
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    // 아래는 nativeQuery=true여서 MySQL일때만 쓸 수 있는 쿼리
+//    @Query(value = "select * from item i where i.item_detail like %:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
--- src/test/java/com/study/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/study/shop/repository/ItemRepositoryTest.java
@@ -1,13 +1,18 @@
 package com.study.shop.repository;
 
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.study.shop.constant.ItemSellStauts;
 import com.study.shop.entity.Item;
+import com.study.shop.entity.QItem;
 import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -18,6 +23,9 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @PersistenceContext // 영속성 컨텍스트 사용하기 위해 EntityManager 빈 주입
+    EntityManager em;
 
     @Test
     @DisplayName("상품 저장 테스트")
@@ -61,6 +69,40 @@ class ItemRepositoryTest {
 
         List<Item> itemList = itemRepository.findByItemName("테스트 상품1");
         for (Item item : itemList) {
+            log.info(item.toString());
+        }
+
+    }
+
+    @Test
+    @DisplayName("JPQL을 이용한 상품 조회 테스트")
+    public void findByItemDetailTest() {
+
+        this.createItemList();
+
+        List<Item> itemList = itemRepository.findByItemDetail("상품 설명");
+        for(Item item : itemList) {
+            log.info(item.toString());
+        }
+
+    }
+
+    @Test
+    @DisplayName("QueryDSL 상품 조회 테스트")
+    public void queryDslTest() {
+
+        this.createItemList();
+
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em); // 생성자 파라미터로 EntityManager 객체 넣어줌
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStauts.eq(ItemSellStauts.SELL))
+                .where(qItem.itemDetail.like("%" + "상품 설명" + "%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();    // fetch를 이용해 쿼리 결과를 리스트로 반환. fetch() 메서드 실행 시점에 쿼리문이 실행됨.
+
+        for(Item item : itemList) {
             log.info(item.toString());
         }
 
