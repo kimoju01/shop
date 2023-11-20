5105be1f059f1f4a61f400a7eb8af535e78f92c9
Hyeju <kimoju03@naver.com>
2023-11-20 20:53:01 +0900

    feat: findByEmail 메서드 Optional로 변경

--- src/main/java/com/study/shop/repository/MemberRepository.java
+++ src/main/java/com/study/shop/repository/MemberRepository.java
@@ -3,8 +3,10 @@ package com.study.shop.repository;
 import com.study.shop.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Member findByEmail(String email);
+    Optional<Member> findByEmail(String email);
 
 }
