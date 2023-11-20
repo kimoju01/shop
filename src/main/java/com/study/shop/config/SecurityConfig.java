f00d92ca8757bf9eef9bbdadf3b8c935d653b7e0
Hyeju <kimoju03@naver.com>
2023-11-20 21:04:26 +0900

    feat: 스프링 시큐리티 로그인, 로그아웃 관련 설정 추가
    Fixes: #9

--- src/main/java/com/study/shop/config/SecurityConfig.java
+++ src/main/java/com/study/shop/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.study.shop.config;
 
+import com.study.shop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,14 +10,27 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final UserDetailsServiceImpl userDetailsService;
 
     // http 요청에 대한 보안 설정
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin()
+                .loginPage("/members/login")    // 로그인 페이지 URL
+                .defaultSuccessUrl("/")         // 로그인 성공 시 이동할 URL
+                .usernameParameter("email")     // 로그인 시 사용할 파라미터 이름
+                .failureUrl("/members/login/error") // 로그인 실패 시 이동할 URL
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))     // 로그아웃 URL
+                .logoutSuccessUrl("/");         // 로그아웃 성공 시 이동할 URL
 
         return http.build();
     }
