eea1b4c64bedd2920b9bfc498e485f6d5e78f1ed
Hyeju <kimoju03@naver.com>
2023-11-22 20:30:28 +0900

    feat: 주문 Orders 엔티티 추가
    - OrderItem 엔티티와 양방향 매핑
    
    Related to: #13

---
+++ src/main/java/com/study/shop/entity/Orders.java
@@ -0,0 +1,42 @@
+package com.study.shop.entity;
+
+import com.study.shop.constant.OrderStatus;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "orders") // order 키워드랑 겹치지않게 orders로 설정
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class Orders extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne  // 한 명의 회원은 여러 번의 주문을 할 수 있다
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;    // 주문한 날짜
+
+    @Column(name = "order_status")
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;    // 주문 상태
+
+    // OrderItem 엔티티와 양방향 매핑을 함
+    // 외래 키가 있는 OrderItem 엔티티의 orders 필드가 관계의 주인이 됨
+    // 주인이 아닌 엔티티에서는 mappedBy로 주인 엔티티의 필드와 매핑되는 것을 나타냄
+    @OneToMany(mappedBy = "orders") // 하나의 주문은 여러 개의 주문 상품을 가질 수 있다
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+}
