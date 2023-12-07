afbbdba669f728c50238a166b1f1defe10ffda95
Hyeju <kimoju03@naver.com>
2023-12-07 19:59:41 +0900

    feat: 상품 이미지 Repository 추가
    Related to: #2

---
+++ src/main/java/com/study/shop/repository/ItemImgRepository.java
@@ -0,0 +1,8 @@
+package com.study.shop.repository;
+
+import com.study.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+}
