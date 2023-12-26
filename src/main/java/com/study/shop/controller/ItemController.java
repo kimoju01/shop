d254e3eb3c1afbf2debb4cfcd2c43b6bc32d51fb
Hyeju <kimoju03@naver.com>
2023-12-26 21:10:00 +0900

    feat: Controller에 상품 수정 페이지 진입하는 itemDetail 메서드와 상품 수정하는 itemUpdate 메서드 작성
    Related to: #2

--- src/main/java/com/study/shop/controller/ItemController.java
+++ src/main/java/com/study/shop/controller/ItemController.java
@@ -7,12 +7,10 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 import java.util.List;
 
@@ -28,7 +26,7 @@ public class ItemController {
         log.info("Item Register GET..........");
 
         model.addAttribute("itemFormDto", new ItemFormDto());
-        return "/item/itemRegister";
+        return "item/itemRegister";
     }
 
     @PostMapping("/admin/item/register")
@@ -58,6 +56,52 @@ public class ItemController {
         }
 
         // 상품이 정상적으로 등록되었다면 메인 페이지로 이동
+        return "redirect:/";
+    }
+
+    @GetMapping("/admin/item/{itemId}")
+    public String itemDetail(@PathVariable("itemId") Long itemId, Model model) {
+        log.info("item Detail GET..........");
+
+        try {
+            ItemFormDto itemFormDto = itemService.getItemDetails(itemId);
+            // 조회한 상품 데이터를 모델에 담아서 뷰로 전달
+            model.addAttribute("itemFormDto", itemFormDto);
+        } catch (EntityNotFoundException e) {
+            // 상품 엔티티가 존재하지 않을 경우 에러 메시지를 담아서 상품 등록 페이지로 이동
+            model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemRegister";
+        }
+
+        return "item/itemModify";
+
+    }
+
+    @PostMapping("/admin/item/{itemId}")
+    public String itemUpdate(@Valid ItemFormDto itemFormDto, BindingResult bindingResult, Model model,
+                             @RequestParam("itemImgFile") List<MultipartFile> itemImgFileList) {
+        log.info("item Update POST..........");
+
+        // 검증 시 에러가 있다면(필수 값이 없다면) 상품 수정 페이지로 이동
+        if (bindingResult.hasErrors()) {
+            return "item/itemModify";
+        }
+
+        // 첫 번째 이미지(대표 이미지)가 없고 상품 id가 없으면 상품 수정 페이지로 이동
+        if (itemImgFileList.get(0).isEmpty() && itemFormDto.getId() == null) {
+            model.addAttribute("errorMessage", "첫 번째 상품 이미지는 필수 입력 값 입니다.");
+            return "item/itemModify";
+        }
+
+        try {
+            itemService.updateItem(itemFormDto, itemImgFileList);
+        } catch (Exception e) {
+            model.addAttribute("errorMessage", "상품 수정 중 에러가 발생했습니다.");
+            return "item/itemModify";
+        }
+
+        // 상품이 정상적으로 수정되었다면 메인 페이지로 이동
         return "redirect:/";
     }
 
